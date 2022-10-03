a5563b1edac2fef8698a3ef3537f2c0bebb78922
alan <769825994@qq.com>
2022-10-03 09:55:05 +0800

    #5 把桶调好了！！！

--- src/main/java/com/renjunxi/mod/bettercopper/Bettercopper.java
+++ src/main/java/com/renjunxi/mod/bettercopper/Bettercopper.java
@@ -2,11 +2,18 @@ package com.renjunxi.mod.bettercopper;
 
 import net.fabricmc.api.ModInitializer;
 import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.fluid.Fluids;
 import net.minecraft.item.*;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
+
+import static com.renjunxi.mod.bettercopper.CopperBukkit.COPPER_BUKKIT;
+import static com.renjunxi.mod.bettercopper.CopperBukkit.COPPER_WATER_BUKKIT;
+
 public class Bettercopper implements ModInitializer {
+
+
     public static ToolItem COPPER_SWORD = new SwordItem(com.renjunxi.mod.bettercopper.CopperMaterial.INSTANCE, 5, 3, new Item.Settings().group(ItemGroup.TOOLS));
     public static ToolItem COPPER_AXE = new com.renjunxi.mod.bettercopper.CopperAxe(com.renjunxi.mod.bettercopper.CopperMaterial.INSTANCE, 8, 2.0F, new Item.Settings().group(ItemGroup.TOOLS));
     public static ToolItem COPPER_PICKAXE = new com.renjunxi.mod.bettercopper.CopperPickAxe(com.renjunxi.mod.bettercopper.CopperMaterial.INSTANCE, 3, 2.0F, new Item.Settings().group(ItemGroup.TOOLS));
@@ -30,8 +37,8 @@ public class Bettercopper implements ModInitializer {
         Registry.register(Registry.ITEM, new Identifier("better_copper", "copper_material_leggings"), COPPER_ARMOR_LEGGINGS);
         Registry.register(Registry.ITEM, new Identifier("better_copper", "copper_material_boots"), COPPER_ARMOR_BOOTS);
 
-        //Registry.register(Registry.ITEM, new Identifier("better_copper", "copper_bukkit"), COPPER_BUKKIT);
-        //Registry.register(Registry.ITEM, new Identifier("better_copper", "copper_water_bukkit"), COPPER_WATER_BUKKIT);
+        Registry.register(Registry.ITEM, new Identifier("better_copper", "copper_bukkit"), COPPER_BUKKIT);
+        Registry.register(Registry.ITEM, new Identifier("better_copper", "copper_water_bukkit"), COPPER_WATER_BUKKIT);
         //Registry.register(Registry.ITEM, new Identifier("better_copper", "copper_lava_bukkit"), COPPER_LAVA_BUKKIT);
     }
 }
--- src/main/java/com/renjunxi/mod/bettercopper/CopperBukkit.java
+++ src/main/java/com/renjunxi/mod/bettercopper/CopperBukkit.java
@@ -1,20 +1,88 @@
 package com.renjunxi.mod.bettercopper;
 
+import net.minecraft.advancement.criterion.Criteria;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.FluidDrainable;
+import net.minecraft.block.FluidFillable;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.fluid.Fluid;
 import net.minecraft.fluid.Fluids;
 import net.minecraft.item.*;
-/*
-public class CopperBukkit implements BucketItem{
-    public static final BucketItem COPPER_BUKKIT=new BucketItem(Fluids.EMPTY, new Item.Settings().group(ItemGroup.MISC));
-    public static final BucketItem COPPER_WATER_BUKKIT=new BucketItem(Fluids.WATER, new Item.Settings().group(ItemGroup.MISC));
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.stat.Stats;
+import net.minecraft.util.Hand;
+import net.minecraft.util.TypedActionResult;
+import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.hit.HitResult;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import net.minecraft.world.RaycastContext;
+import net.minecraft.world.World;
+import net.minecraft.world.event.GameEvent;
 
-    public Item fixBucketItem(Item bucketIn) {
-        if (bucketIn == Items.WATER_BUCKET) {
-            return (Item)COPPER_WATER_BUKKIT;
-        } else if (bucketIn == Items.MILK_BUCKET) {
-            return (Item)COPPER_BUKKIT;
+public class CopperBukkit extends BucketItem {
+    public static final BucketItem COPPER_BUKKIT=new CopperBukkit(Fluids.EMPTY, new Item.Settings().group(ItemGroup.MISC));
+    public static final BucketItem COPPER_WATER_BUKKIT=new CopperBukkit(Fluids.WATER, new Item.Settings().group(ItemGroup.MISC));
+    private final Fluid fluid;
+
+    public CopperBukkit(Fluid fluid, Item.Settings settings) {
+        super(fluid,settings);
+        this.fluid = fluid;
+    }
+
+
+    public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
+        ItemStack itemStack = user.getStackInHand(hand);
+        BlockHitResult blockHitResult = raycast(world, user, this.fluid == Fluids.EMPTY ? RaycastContext.FluidHandling.SOURCE_ONLY : RaycastContext.FluidHandling.NONE);
+        if (blockHitResult.getType() == HitResult.Type.MISS) {
+            return TypedActionResult.pass(itemStack);
+        } else if (blockHitResult.getType() != HitResult.Type.BLOCK) {
+            return TypedActionResult.pass(itemStack);
         } else {
-            return bucketIn == Items.LAVA_BUCKET ? null : bucketIn;
+            BlockPos blockPos = blockHitResult.getBlockPos();
+            Direction direction = blockHitResult.getSide();
+            BlockPos blockPos2 = blockPos.offset(direction);
+            if (world.canPlayerModifyAt(user, blockPos) && user.canPlaceOn(blockPos2, direction, itemStack)) {
+                BlockState blockState;
+                if (this.fluid == Fluids.EMPTY) {
+                    blockState = world.getBlockState(blockPos);
+                    if (blockState.getBlock() instanceof FluidDrainable) {
+                        FluidDrainable fluidDrainable = (FluidDrainable)blockState.getBlock();
+                        ItemStack itemStack2 = fluidDrainable.tryDrainFluid(world, blockPos, blockState);
+                        if (!itemStack2.isEmpty()) {
+                            user.incrementStat(Stats.USED.getOrCreateStat(this));
+                            fluidDrainable.getBucketFillSound().ifPresent((sound) -> {
+                                user.playSound(sound, 1.0F, 1.0F);
+                            });
+                            world.emitGameEvent(user, GameEvent.FLUID_PICKUP, blockPos);
+                            //ItemStack itemStack3 = ItemUsage.exchangeStack(itemStack, user, itemStack2);
+                            if (!world.isClient) {
+                                Criteria.FILLED_BUCKET.trigger((ServerPlayerEntity)user, itemStack2);
+                            }
+
+                            return TypedActionResult.success(new ItemStack(COPPER_WATER_BUKKIT), world.isClient());
+                        }
+                    }
+
+                    return TypedActionResult.fail(itemStack);
+                } else {
+                    blockState = world.getBlockState(blockPos);
+                    BlockPos blockPos3 = blockState.getBlock() instanceof FluidFillable && this.fluid == Fluids.WATER ? blockPos : blockPos2;
+                    if (this.placeFluid(user, world, blockPos3, blockHitResult)) {
+                        this.onEmptied(user, world, itemStack, blockPos3);
+                        if (user instanceof ServerPlayerEntity) {
+                            Criteria.PLACED_BLOCK.trigger((ServerPlayerEntity)user, blockPos3, itemStack);
+                        }
+
+                        user.incrementStat(Stats.USED.getOrCreateStat(this));
+                        return TypedActionResult.success(!user.getAbilities().creativeMode ? new ItemStack(COPPER_BUKKIT) : itemStack, world.isClient());
+                    } else {
+                        return TypedActionResult.fail(itemStack);
+                    }
+                }
+            } else {
+                return TypedActionResult.fail(itemStack);
+            }
         }
     }
 }
-*/
